950a9d4479dc84d849bc49db8040059f9013a05d
Priya Thakur <74084538+priyathakur1234@users.noreply.github.com>
2023-06-26 20:01:57 +0545

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <header>
+    <div id="header">
+    <h1>Contact us</h1>
+    <h2>Contact the Net Ninja</h2>
+
+    </div>
+
+    <h2 id="content-header">content</h2>
+   
+
+
+   <p>Hello there ninjas!</p>  
+   <p>I am here</p> 
+
+   <a href="http://www.google.com">Google</a>
+<span>yo I am fine</span>
+   <span class="deck">I am Fine</span>
+   <span class="deck">I am Fine</span>
+   <span class="deck">I am Fine</span>
+   
+
+   </header>
+  );
+}
+
+export default App;
